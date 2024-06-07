657a3b4094363f8afffce1bca3d9090df8fbc325
leen Awad <leen@gmail.com>
2024-06-07 16:52:31 +0300

    add meal and ingredients api

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -43,6 +43,7 @@ class DatabaseSeeder extends Seeder
             'email'=>'karamalmadne@gmail.com',
             'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi' // password
         ]);
-
+        $this->call(MealSeeder::class);
+        $this->call(IngredientSeeder::class);
     }
 }
