463dcd0113d2e47534f7c0cbba0dd0dfcc13f916
leen Awad <leen@gmail.com>
2024-08-04 00:47:58 +0300

    report

--- app/Http/Controllers/api/V1/ReportController.php
+++ app/Http/Controllers/api/V1/ReportController.php
@@ -24,20 +24,26 @@ class ReportController extends Controller
                 'errors' => $validator->errors()
             ], 422);
         }
+
         $userId = $request->user()->id;
         $today = Carbon::today();
 
         $report = Report::where('user_id', $userId)
-                        ->where('report_date', $today)
-                        ->first();
+            ->where('report_date', $today)
+            ->first();
 
         if ($report) {
+
+            $existingTotalTimeSeconds = $this->timeToSeconds($report->total_time);
+            $newTotalTimeSeconds = $this->timeToSeconds($request->total_time);
+
             $report->update([
-                'calories' => $request->calories,
-                'Number_of_exercises' => $request->Number_of_exercises,
-                'total_time' => $request->total_time,
+                'calories' => $report->calories + $request->calories,
+                'Number_of_exercises' => $report->Number_of_exercises + $request->Number_of_exercises,
+                'total_time' => $this->secondsToTime($existingTotalTimeSeconds + $newTotalTimeSeconds),
             ]);
         } else {
+
             Report::create([
                 'user_id' => $userId,
                 'report_date' => $today,
@@ -50,4 +56,19 @@ class ReportController extends Controller
         return response()->json(['message' => 'Report saved successfully']);
     }
 
+
+    private function timeToSeconds($time)
+    {
+        list($hours, $minutes, $seconds) = explode(':', $time);
+        return ($hours * 3600) + ($minutes * 60) + $seconds;
+    }
+
+    private function secondsToTime($seconds)
+    {
+        $hours = floor($seconds / 3600);
+        $minutes = floor(($seconds % 3600) / 60);
+        $seconds = $seconds % 60;
+
+        return sprintf('%02d:%02d:%02d', $hours, $minutes, $seconds);
+    }
 }
